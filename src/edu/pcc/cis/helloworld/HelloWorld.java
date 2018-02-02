e28dfd475c3b654ec712a74137073e7ac8c8cb47
Andrew Bartsch <andrew.bartsch@pcc.edu>
2018-02-01 18:16:57 -0800

    added main method that prints hello world

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,4 +1,10 @@
 package edu.pcc.cis.helloworld;
 
+/**
+ * created by andrew Bartsch on 2/1/18
+ */
 public class HelloWorld {
+    public static void main (String[] args){
+        System.out.println("Hello, World!");
+    }
 }
